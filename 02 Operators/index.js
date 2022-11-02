cadfa20f5b055cded39b76908b0f871380844464
Ajeet Singh <ajrana70@gmail.com>
2022-11-03 02:20:37 +0530

    js basics

---
+++ 02 Operators/index.js
@@ -0,0 +1,15 @@
+const a = 5;
+const b = 3;
+const z = a + b;
+
+console.log(
+  `The value of a => ${a} \n The value of b => ${b} \n The sum of a and b => ${z}`
+);
+console.log(
+    `The value of a => ${a} \n The value of b => ${b} \n The subtraction of a and b => ${a-b}`
+  );
+  
+  console.log(
+    `The value of a => ${a} \n The value of b => ${b} \n The multiplication of a and b => ${a*b}`
+  );
+  
---
+++ 03 Math function/index.js
@@ -0,0 +1,7 @@
+// Ceil
+const x = 6.7;
+console.log(`The ceil value of ${x} is ${Math.ceil(x)}`)
+
+// Floor
+console.log(`The floor value of ${x} is ${Math.floor(x)}`)
+
